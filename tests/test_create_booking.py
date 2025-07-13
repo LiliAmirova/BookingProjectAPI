90b8c5efa27d027961ba473b88f3b27f28b86ae7
Lilia <lg.amirova79@gmail.com>
2025-07-13 21:59:22 +0300

    Урок №5: Добавила тесты на метод Ping + тест на метод CreateBooking

---
+++ tests/test_create_booking.py
@@ -0,0 +1,33 @@
+import allure
+import pytest
+import requests
+from conftest import api_client, generate_random_booking_data
+
+@allure.feature('Test Booking')
+@allure.story('Test CreateBooking')
+def test_create_booking(generate_random_booking_data, api_client):
+    with allure.step("Подготовка данных для  создания брони"):
+        booking_data = generate_random_booking_data
+
+    with allure.step("Отправка запроса на создание бронирования"):
+        response = requests.post("https://restful-booker.herokuapp.com/booking", json=booking_data)
+        #response = api_client.create_booking(booking_data)
+        response_json = response.json()
+
+    with allure.step("Проверка статуса ответа"):
+        assert response.status_code == 200, f"Ожидался статус 200, получен {response.status_code}"
+
+    with allure.step("Проверка параметров брони в ответе"):
+        assert 'bookingid' in response_json, "В ответе отсутствует bookingid"
+        assert isinstance(response_json['bookingid'], int), "bookingid должен быть числом"
+        assert response_json['booking']['firstname'] == booking_data['firstname'], "firstname не совпадает с ожидаемым"
+        assert response_json['booking']['lastname'] == booking_data['lastname'], "lastname не совпадает с ожидаемым"
+        assert response_json['booking']['totalprice'] == booking_data['totalprice'], "totalprice не совпадает с ожидаемым"
+        assert response_json['booking']['depositpaid'] == booking_data['depositpaid'], "depositpaid не совпадает с ожидаемым"
+        assert response_json['booking']['bookingdates']['checkin'] == booking_data['bookingdates']['checkin'], "checkin не совпадает с ожидаемым"
+        assert response_json['booking']['bookingdates']['checkout'] == booking_data['bookingdates']['checkout'], "checkout не совпадает с ожидаемым"
+        assert response_json['booking']['additionalneeds'] == booking_data['additionalneeds'], "additionalneeds не совпадает с ожидаемым"
+
+
+
+
